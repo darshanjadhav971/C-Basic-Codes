6da41523d0b3e95becca4fed6282eaca09c57bde
darshanjadhav971 <darshanjadhav971@gmail.com>
2025-08-22 20:54:19 +0700

    [ADDED]: Darshan - C Program for Switch Case

---
+++ Switch.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+int main()
+{
+	int day;
+	printf("Enter a Number (1-7) \n");
+	scanf("%d",&day);
+	
+	switch(day)
+	{
+		case 1:
+			printf("Sunday \n");
+			break;
+			
+		case 2:	
+			printf("Monday \n");
+			break;
+			
+		case 3:
+			printf("Tuesday \n");
+			break;
+			
+		case 4:
+			printf("Wednesday \n");
+			break;
+		
+		case 5:
+			printf("Thursday \n");
+			break;
+			
+		case 6:
+			printf("Friday \n");
+			break;	
+			
+		case 7:
+			printf("Saturday \n");
+			break;	
+			
+		default:
+		printf("Invalid input Please Enter a Number between: (1 and 7) \n");	
+		
+	}
+	return 0;
+}
