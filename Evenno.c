e2e9701a00fbac3225a5c91bd00965a06a772bcc
darshanjadhav971 <darshanjadhav971@gmail.com>
2025-08-22 20:46:09 +0700

    Add files via upload

---
+++ Evenno.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+	int main()
+	{
+		int e;
+		printf("Enter the Even No: \n");
+		scanf("%d",&e);
+		
+		if(e%2==0){
+			
+			printf("Number is Even \n");
+		}
+		else{
+			
+			printf("Number is Odd \n");
+		}
+		return 0;
+}
